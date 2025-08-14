1c634c21ad5ac657498f6c6d98fddd6eba6bb576
Rasika Bhalerao <rasikabh@users.noreply.github.com>
2025-08-14 17:37:53 -0400

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,7 +1,12 @@
-# When we process text in machine learning, we often use character ngrams instead of entire words, to avoid dealing with punctuation and typos.
-# For example, the text "Rasika", with a value of n=4, has these character ngrams: "Rasi", "asik", and "sika".
-# Each of these character ngrams appears once in "Rasika", and each of them appears twice in "RasikaRasika"
-# (and "RasikaRasika" also has the additional character ngrams "ikaR", "kaRa", and "aRas", each of which appears once).
+"""
+When we process text in machine learning, we often use character ngrams instead of entire words, to avoid dealing with punctuation and typos.
+For example, the text "Rasika", with a value of n=4, has these character ngrams: "Rasi", "asik", and "sika".
+Each of these character ngrams appears once in "Rasika", and each of them appears twice in "RasikaRasika"
+(and "RasikaRasika" also has the additional character ngrams "ikaR", "kaRa", and "aRas", each of which appears once).
+
+Please implement count_char_n_grams() below according to the comments describing its functionality.
+You will need to write the documentation.
+"""
 
 def count_char_n_grams(text: str, n: int = 3) -> dict[str, int]:
     # Very similar to count_words(), but instead of counting entire words, it counts each sequence of n characters.
